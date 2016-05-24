c847770b027bdfbb1856e4bf3a41054be64648dd
h0rror <spiceace@gmail.com>
2016-05-24 14:19:57 -0400

    updated person class to include name property and 2 constructors

--- Lab2/lab2/Person.cs
+++ Lab2/lab2/Person.cs
@@ -6,8 +6,63 @@ using System.Threading.Tasks;
 
 namespace lab2
 {
+    /**
+     * REFER TO http://yui.github.io/yuidoc/syntax/index.html for documentation syntax conventions
+     * This class defines a generic Person
+     * 
+     * @class Person
+     * @field _name {string}
+     */
     public class Person
     {
+        // PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //(underscores signify private variables)  
+        //[always add an access modifier likepublic or private to variables]
+        private string _name;
 
+        //PUBLIC PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //(no underscore and capitalized first letter to signify public)
+
+        /**
+         * This is a property for out _name field
+         *
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+           
+            set
+            {
+                this._name = value;
+
+            }
+           
+        }
+        /**
+         *This is the default empty constructor for the Person class
+         *
+         * @constructor Person
+         */
+        public Person()
+        {
+            this._name = "unknown name!";
+
+        }
+        /**
+      *This is a constructor that takes name as a parameter and passes it
+      *to the _name private instance variable
+      *
+      * @constructor Person
+      * @param {string} name
+      */
+        public Person(string name)
+        {
+            this._name = name;
+
+        }
     }
 }
--- Lab2/lab2/Program.cs
+++ Lab2/lab2/Program.cs
@@ -6,11 +6,36 @@ using System.Threading.Tasks;
 
 namespace lab2
 {
+    /**
+     * This class is the "driver" class for our program
+     * 
+     * @class Program
+     * */
     public class Program
     {
+
+        /**
+         * the main method for out driver class
+         * 
+         * @method Main
+         * @param {string[]} args
+         * */
         public static void Main(string[] args)
         {
+            Person person; // Person person = new Person(); <--- way to do it in one line
+
+            // create a new instance of the Person class
+            person = new Person("Tom");
+
+            Console.WriteLine(person.Name);
+
+            // set a value in the Name property of the Person class
+           // person.Name = "Tom";
+
+            // get the Name value and print to console
+            Console.WriteLine(person.Name);
             
+
         }
     }
 }
